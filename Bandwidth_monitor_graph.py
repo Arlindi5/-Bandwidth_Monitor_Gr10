0861ec0d9eebb60066f42636cd7fc51d33775943
Dredhza Braina <94399125+DredhzaBraina@users.noreply.github.com>
2023-01-06 20:30:34 +0100

    Update Bandwidth_monitor_graph.py

--- Bandwidth_monitor_graph.py
+++ Bandwidth_monitor_graph.py
@@ -12,3 +12,36 @@ DATA_DURATION = 5
 speeds_recv = []
 speeds_sent = []
 times = []
+
+def table():
+    subprocess.call("Bandwidth_monitor_table.py", shell=True)
+
+def graph():
+    """ Executes the program.
+    """
+    tracker = Tracker()
+    plt.ion()
+
+    while True:
+        # Retrieve the up and down speeds
+        time.sleep(0.5)
+        down_speed = tracker.get_current_download_speed() / (2**20)
+        up_speed = tracker.get_current_upload_speed() / (2**20)
+
+        # Store it
+        add_data(down_speed, up_speed)
+
+        # Update & display the plot
+        recv_curve, = plt.plot(times, speeds_recv)
+        sent_curve, = plt.plot(times, speeds_sent)
+
+        plt.legend([recv_curve, sent_curve], ['Download', 'Upload'])
+        plt.ylabel('MB/s', fontsize=8)
+        ax = plt.gca()
+        ax.tick_params(axis='x', labelsize=6)
+        ax.tick_params(axis='y', labelsize=6)
+        ax.get_figure().canvas.set_window_title('Bandwidth monitor')
+
+        plt.draw()
+        plt.pause(0.0001)
+        plt.clf()
